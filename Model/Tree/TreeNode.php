33d155afaf86c16be98eb188b7068844537db29a
nazin <nazinorbi@gmail.com>
2026-08-01 20:24:26 +0200

    TreeNode nagyon sok függvényel való bővítése

--- Model/Tree/TreeNode.php
+++ Model/Tree/TreeNode.php
@@ -67,15 +67,126 @@ class TreeNode implements TreeNodeInterface
 
 
     /**
-     * A részfa mérete csomópontokban.
+     * Gyökér vizsgálata.
      */
-    public function getSubtreeSize(): int
+    public function isRoot(): bool
     {
-        return intdiv(
-            $this->right - $this->left + 1,
-            2
-        );
+        return $this->level === 0;
     }
+
+    /**
+     * Fa információk
+     */
+    public function getRoot(): TreeNodeInterface {
+
+    }
+
+    public function isEmpty(): bool {
+
+    }
+
+    public function count(): int {
+
+    }
+
+    public function getMaxDepth(): int {
+
+    }
+
+    /**
+     * Csomópont keresés
+     */
+
+    public function getNode(int|string $id): ?TreeNodeInterface {
+
+    }
+
+    public function hasNode(int|string $id): bool {
+
+    }
+
+    public function find(callable $predicate): ?TreeNodeInterface {
+
+    }
+
+    /**
+     * @return TreeNodeInterface[]
+     */
+    public function findAll(callable $predicate): array {
+
+    }
+
+    /**
+     * Strukturális lekérdezések
+     */
+
+    public function getParent(TreeNodeInterface $node): ?TreeNodeInterface {
+
+    }
+
+    /**
+     * @return TreeNodeInterface[]
+     */
+    public function getChildren(TreeNodeInterface $node): array {
+
+    }
+
+    /**
+     * @return TreeNodeInterface[]
+     */
+    public function getDescendants(TreeNodeInterface $node): array {
+
+    }
+
+    /**
+     * @return TreeNodeInterface[]
+     */
+    public function getAncestors(TreeNodeInterface $node): array {
+
+    }
+
+    /**
+     * @return TreeNodeInterface[]
+     */
+    public function getSiblings(TreeNodeInterface $node): array {
+
+    }
+
+    /**
+     * Biólógiai lekérdezések
+     */
+
+    /**
+     * @return TreeNodeInterface[]
+     */
+    public function getLeaves(): array {
+
+    }
+
+    /**
+     * @return TreeNodeInterface[]
+     */
+    public function getExtinctTaxa(): array {
+
+    }
+
+    /**
+     * @return TreeNodeInterface[]
+     */
+    public function getLivingTaxa(): array {
+
+    }
+
+    /**
+     * @return TreeNodeInterface[]
+     */
+    public function getByRank(TaxonomicRankEnum $rank): array {
+
+    }
+
+    /**
+     * Nested Set számítások
+     */
 
     /**
      * A csomópont szélessége Nested Set érték alapján.
@@ -86,11 +197,78 @@ class TreeNode implements TreeNodeInterface
     }
 
     /**
-     * Gyökér vizsgálata.
+     * A részfa mérete csomópontokban.
      */
-    public function isRoot(): bool
+    public function getSubtreeSize(): int
     {
-        return $this->level === 0;
+        return intdiv(
+            $this->right - $this->left + 1,
+            2
+        );
+    }
+
+    public function isAncestor(
+        TreeNodeInterface $ancestor,
+        TreeNodeInterface $node
+    ): bool {
+
+    }
+
+    public function isDescendant(
+        TreeNodeInterface $node,
+        TreeNodeInterface $ancestor
+    ): bool {
+
+    }
+
+    /**
+     * Módósítások
+     */
+    public function add(TreeNodeInterface $node): void {
+
+    }
+
+    public function remove(TreeNodeInterface $node): void {
+
+    }
+
+    public function move(
+        TreeNodeInterface $source,
+        TreeNodeInterface $destination
+    ): void {
+
+    }
+
+    public function copy(
+        TreeNodeInterface $source,
+        TreeNodeInterface $destination
+    ): TreeNodeInterface {
+
+    }
+
+    /**
+     * Validáció
+     */
+    public function validate(): bool {
+
+    }
+
+    public function isValid(): bool {
+
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getValidationErrors(): array {
+
+    }
+
+    /**
+     * Iteráció
+     */
+    public function getIterator(): Traversable {
+
     }
 
 }
